0638654d776dc4eba71f9a88335f65d99be79ff6
shamshod1012 <toshpulatovshamshodbek@gmail.com>
2023-04-27 16:18:54 +0500

    yangi o'zgartirishlar

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,32 @@
 import React from "react";
 
-export const Card = () => {
-  return <div>Card</div>;
+import {
+  AiOutlinePlusSquare,
+  AiOutlineHeart,
+  AiFillHeart,
+} from "react-icons/ai";
+
+import "./Card.css";
+
+export const Card = ({ id, title, image, price }) => {
+  return (
+    <div className="cardContainer">
+      <div className="cardHeader">
+        <div className="">
+          <AiOutlineHeart className="emptyHeart" size={24} />
+        </div>
+        <img src={image} alt="" />
+      </div>
+
+      <div className="cardMain">{title}</div>
+
+      <div className="cardFooter">
+        <div className="cardPrice">
+          <span>Цена:</span>
+          <p>{price} руб</p>
+        </div>
+        <AiOutlinePlusSquare className="plusIcon" size={30} />
+      </div>
+    </div>
+  );
 };
