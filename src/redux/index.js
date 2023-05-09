9a199cf36225e81c79bb988b56a09684c4dba19b
shamshod1012 <toshpulatovshamshodbek@gmail.com>
2023-05-09 17:58:47 +0500

    new changings

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,4 +1,6 @@
 import { createStore } from "redux";
 import { sneakesReducer } from "./sneakersReducer";
+import thunk from "redux-thunk";
+import { applyMiddleware } from "redux";
 
-export const store = createStore(sneakesReducer);
+export const store = createStore(sneakesReducer, applyMiddleware(thunk));
